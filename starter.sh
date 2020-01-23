566d555c1a2f29b77f7f7ff441b87ca4906d640a
Jack Laxson <jackjrabbit@gmail.com>
2020-01-23 16:57:55 -0500

    work on seperating gui apps

--- starter.sh
+++ starter.sh
@@ -1,16 +1,29 @@
-sudo apt-get install git docker.io terminator emacs redshift-gtk vlc npm htop nodejs-legacy gitg python-pip ngrok-client httpie xclip mosh \
-    zsh ipython gnome-shell-pomodoro ack-grep virtualenv ruby-bcat twine deluge darktable apt-listbugs gist pcscd scdaemon pcsc-tools secure-delete \
-    direnv hub yadm flameshot
+#!/bin/bash
+
+function main_apps {
+    sudo apt install git emacs npm htop python-pip httpie xclip mosh \
+    zsh ipython ack-grep virtualenv ruby-bcat twine apt-listbugs gist pcscd scdaemon pcsc-tools secure-delete \
+    direnv hub yadm mtr-tiny postgresql-client ruby
+}
 
 # TODO: Add user to docker group
 
-#steam deps
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6-i368 libgl1-nvidia-glx:i386
+function steam_32bit_setup {
+    #steam deps 32bit old
+    sudo dpkg --add-architecture i386
+    sudo apt update
+    sudo apt-get install libc6-i368 libgl1-nvidia-glx:i386
 
-sudo apt-get -o Dpkg::Options::="--force-overwrite" install libuuid1:i386 
-# Fixes stellaris
+    sudo apt-get -o Dpkg::Options::="--force-overwrite" install libuuid1:i386 
+    # Fixes stellaris
+}
+
+function gui_extras {
+    #latex
+    sudo apt install org-mode lyx vlc gitg xclip gnome-shell-pomodoro deluge darktable flameshot terminator
+}
+
+mainapps
 
 #oh my zshell
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -18,15 +31,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 #js globals
 #sudo npm install -g bower
 
+# does this still work? 1/2020
 sudo pip install vanity
 
-# Configure!!
-# `source /usr/local/bin/activate.sh` in .zshrc
-
-#heroku 
-#wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
-sudo apt-get install postgresql-client ruby
-
-# latex stuff optional
-sudo apt-get install org-mode lyx
+gui_extras
