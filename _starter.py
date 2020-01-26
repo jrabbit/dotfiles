e67d7014e26e4f625e8fa7bb4f85b360a4c7bfe2
Jack Laxson <jackjrabbit@gmail.com>
2020-01-26 05:20:02 -0500

    add helpter script for packages

---
+++ _starter.py
@@ -0,0 +1,19 @@
+#!/bin/env python3
+import sys
+import logging
+from pathlib import Path
+from pprint import pformat
+
+import toml
+
+p = Path("~/.config/jack.laxson", "packages.toml")
+pkgs = toml.load(p.expanduser())
+
+def to_string():
+    print(f"sudo apt install {' '.join(pkgs['packages'])}")
+    print(f"sudo apt install {' '.join(pkgs['gui_packages'])}")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug(pformat(pkgs))
+    to_string()
