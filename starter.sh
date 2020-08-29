a132fcd1f01940bb60aba9e02b206c7634c0d716
Jack Laxson <jackjrabbit@gmail.com>
2020-08-29 05:23:02 -0400

    more automation

--- starter.sh
+++ starter.sh
@@ -26,11 +26,13 @@ function gui_extras {
 main_apps
 
 #oh my zshell
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 function pydev {
 	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 	sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-	curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+	curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
+	mkdir $ZSH/plugins/poetry
+	poetry completions zsh > $ZSH/plugins/poetry/_poetry
 }
 
 gui_extras
