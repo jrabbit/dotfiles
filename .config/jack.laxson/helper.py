d697aaabf1967d94f217bf3d2cffcb632a947860
Jack Laxson <jackjrabbit@gmail.com>
2020-11-19 02:43:21 -0500

    relative file slickwise

--- .config/jack.laxson/helper.py
+++ .config/jack.laxson/helper.py
@@ -1,9 +1,10 @@
 import sys
 import subprocess
+from pathlib import Path
 import toml
 
 if __name__ == "__main__":
-    t = toml.load("docker-base-images.toml")
+    t = toml.load(Path(__file__).with_name("docker-base-images.toml"))
     for x in t['images']:
         prog = sys.argv[1:]
         prog.append(x)
