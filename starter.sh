284c6fe2701b19b4c0cfbfcc947869d3ebdaa752
Jack Laxson <jackjrabbit@gmail.com>
2021-04-30 23:30:12 -0400

    command won't work w/o this

--- starter.sh
+++ starter.sh
@@ -45,6 +45,7 @@ function pydev {
 	sudo apt-get install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 	curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 	mkdir $ZSH/plugins/poetry
+	source $HOME/.poetry/env
 	poetry completions zsh > $ZSH/plugins/poetry/_poetry
 }
 
